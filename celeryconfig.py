4f7614aa264d7d549a7c5cf480fb74cae0749480
eon01 <amri.aymen@gmail.com>
2015-06-23 12:37:03 +0200

    initial commit

---
+++ celeryconfig.py
@@ -0,0 +1,16 @@
+CELERY_IMPORTS = ('tasks')
+CELERY_IGNORE_RESULT = False
+BROKER_HOST = "127.0.0.1"
+BROKER_PORT = 5672
+BROKER_URL='amqp://'
+CELERY_RESULT_BACKEND = "amqp"
+CELERY_IMPORTS=("tasks",)
+from celery.schedules import crontab
+ 
+CELERYBEAT_SCHEDULE = {
+    'every-minute': {
+        'task': 'tasks.multiply',
+        'schedule': crontab(minute='*/1'),
+        'args': (1,2),
+    },
+}
